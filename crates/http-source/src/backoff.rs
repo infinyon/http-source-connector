d187ebc83abfb1a8c544dac1139ee825820daec7
Carson Rajcan <carajcan@gmail.com>
2024-02-08 11:12:51 -0600

    Add backoff for reconnect

---
+++ crates/http-source/src/backoff.rs
@@ -0,0 +1,19 @@
+pub(crate) enum Backoff {
+    Fib(u64, u64)
+}
+
+impl Backoff {
+    pub fn new() -> Self {
+        Backoff::Fib(1, 1)
+    }
+
+    pub fn next(&mut self) -> u64 {
+        match self {
+            Backoff::Fib(a, b) => {
+                let next = *a + *b;
+                *self = Backoff::Fib(*b, next);
+                next
+            }
+        }
+    }
+}
--- crates/http-source/src/main.rs
+++ crates/http-source/src/main.rs
@@ -2,17 +2,20 @@ mod config;
 mod formatter;
 mod http_streaming_source;
 mod source;
+mod backoff;
 
 use anyhow::Result;
 use async_std::stream::StreamExt;
+use backoff::Backoff;
 use config::HttpConfig;
 use fluvio::{RecordKey, TopicProducer};
 use fluvio_connector_common::{
     connector,
-    tracing::{debug, info, trace},
+    tracing::{error, debug, info, trace},
     Source,
 };
 
+use futures::Stream;
 use http_streaming_source::HttpStreamingSource;
 use source::HttpSource;
 
@@ -22,16 +25,14 @@ const SIGNATURES: &str = concat!("InfinyOn HTTP Source Connector ", env!("CARGO_
 #[allow(unreachable_code)]
 async fn start(config: HttpConfig, producer: TopicProducer) -> Result<()> {
     debug!(?config);
+    let mut backoff = Backoff::new();
 
     loop {
         let mut stream = if config.stream {
-            match HttpStreamingSource::new(&config)?.connect(None).await {
+            match connect_streaming_source(&config, &mut backoff).await {
                 Ok(stream) => stream,
-                Err(err) => {
-                    info!("Error connecting to streaming source: {}", err);
-                    //sleep 500ms before reconnecting
-                    async_std::task::sleep(std::time::Duration::from_millis(500)).await;
-                    continue;
+                Err(_err) => {
+                   continue;
                 }
             }
         } else {
@@ -49,4 +50,24 @@ async fn start(config: HttpConfig, producer: TopicProducer) -> Result<()> {
     }
 
     Ok(())
+}
+
+
+async fn connect_streaming_source(config: &HttpConfig, backoff: &mut Backoff) -> Result<std::pin::Pin<Box<dyn Stream<Item = String>>>> {
+    match HttpStreamingSource::new(config)?.connect(None).await {
+        Ok(stream) => Ok(stream),
+        Err(err) => {
+            info!("Error connecting to streaming source: {}", err);
+
+            let wait = backoff.next();
+
+            if wait > 10000 {
+                error!("Failed to connect to streaming source after 20 retries");
+            }
+
+            async_std::task::sleep(std::time::Duration::from_millis(100 * wait)).await;
+
+            Err(err)
+        }
+    }
 }
